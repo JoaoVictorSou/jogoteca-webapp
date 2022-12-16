cdfa5a6bf808b5939e4fc9b35d5b71052007d059
JoaoVictorSou <soulima.joao@gmail.com>
2022-12-16 12:03:48 -0300

    games registrator

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-from flask import Flask, render_template
-from source.models.jogo import Jogo
+from flask import Flask, render_template, redirect, request
+from source.models.jogo import Jogo, lista_jogos
 
 # O __name__ faz referência ao próprio módulo.
 app = Flask(__name__)
 
 # Rotas
-
 @app.route('/')
-def homepage():
-    jogo_01 = Jogo("God of War Ragnarok", ["Ação", "Aventura"], ["Playstation 4", "Playstation 5", "PC"])
-    jogo_02 = Jogo("Elden Ring", "RPG", ["Playstation 4", "Playstation 5", "Xbox One", "Xbox One X/S", "PC"])
-    jogo_03 = Jogo("The Legend of Zelda Breath of The Wild", "RPG", ["Nintendo Switch", "Wii U"])
-
-    print(type(jogo_02.categoria) == list)
-
-    lista_jogos = [
-        jogo_01,
-        jogo_02,
-        jogo_03
-    ]
-
+def index():
+    
     return render_template("list.html", titulo = 'Jogos', jogos = lista_jogos)
 
+@app.route('/game/register', methods = ["GET",])
+def show_register_game_page():
+    return render_template('new_game.html', titulo = 'Novo Jogo')
+
+@app.route('/game/create', methods = ["POST",])
+def create_game():
+    print(request)
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    lista_jogos.append(
+        Jogo(nome, categoria, console)
+    )
+
+    return redirect('/')
+
+# Inicia servidor
 if __name__ == "__main__":
-    app.run(host = '127.0.0.1', port=8080)
+    app.run(host = '127.0.0.1', port=8080, debug=True)
--- source/models/jogo.py
+++ source/models/jogo.py
@@ -3,3 +3,13 @@ class Jogo:
         self.nome = str(nome)
         self.categoria = categoria if type(categoria) == list else [categoria]
         self.console = console if type(console) == list else [console]
+
+jogo_01 = Jogo("God of War Ragnarok", ["Ação", "Aventura"], ["Playstation 4", "Playstation 5", "PC"])
+jogo_02 = Jogo("Elden Ring", "RPG", ["Playstation 4", "Playstation 5", "Xbox One", "Xbox One X/S", "PC"])
+jogo_03 = Jogo("The Legend of Zelda Breath of The Wild", "RPG", ["Nintendo Switch", "Wii U"])
+
+lista_jogos = [
+    jogo_01,
+    jogo_02,
+    jogo_03
+]
