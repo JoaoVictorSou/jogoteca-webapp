9cb668a018df78e1d44364f0a63ee319f724fdeb
JoaoVictorSou <soulima.joao@gmail.com>
2022-12-23 06:04:51 -0300

    login route

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, flash, session, request
 from source.models.jogo import Jogo, lista_jogos
 
 # O __name__ faz referência ao próprio módulo.
 app = Flask(__name__)
-
+app.secret_key = 'Nintendo'
 # Rotas
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/authenticate', methods = ['POST',])
+def authenticate():
+    if request.form['senha'] == 'alohomora':
+        session['usuario_logado'] = request.form['usuario']
+        flash('Usuário logado com sucesso!')
+        
+        return redirect('/')
+    
+    flash('Usuário não logado.')
+    return redirect('/login')
+
 @app.route('/')
 def index():
     
