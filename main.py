dd5086d8bea7a5bebe6d940323191584d8d9da9b
JoaoVictorSou <soulima.joao@gmail.com>
2022-12-23 15:33:46 -0300

    dinamic URLs in redirect functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, flash, session, request
+from flask import Flask, render_template, redirect, flash, url_for, session, request
 from source.models.jogo import Jogo, lista_jogos
 
 # O __name__ faz referência ao próprio módulo.
@@ -7,18 +7,38 @@ app.secret_key = 'Nintendo'
 # Rotas
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    next_page = request.args.get('next')
+
+    return render_template(f'login.html', next_page = next_page)
 
 @app.route('/authenticate', methods = ['POST',])
 def authenticate():
+    # The None type converted
+    next_page = request.form.get('next')
+    next_page = next_page if next_page != "None" else None 
+
     if request.form['senha'] == 'alohomora':
         session['usuario_logado'] = request.form['usuario']
         flash('Usuário logado com sucesso!')
         
-        return redirect('/')
+        print(type(next_page))
+
+        if next_page:
+            return redirect(f'/{next_page}')
+        
+        return redirect(url_for('index'))
     
     flash('Usuário não logado.')
-    return redirect('/login')
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+
+    flash("Usuário desconectado.")
+
+    return redirect(url_for('login'))
+
 
 @app.route('/')
 def index():
@@ -27,6 +47,11 @@ def index():
 
 @app.route('/game/register', methods = ["GET",])
 def show_register_game_page():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash("Faça login antes de adicionar jogos.")
+        
+        return redirect(url_for('login', next='game/register'))
+    
     return render_template('new_game.html', titulo = 'Novo Jogo')
 
 @app.route('/game/create', methods = ["POST",])
@@ -41,14 +66,6 @@ def create_game():
     )
 
     return redirect('/')
-
-@app.route('/logout')
-def logout():
-    session['usuario_logado'] = None
-
-    flash("Usuário desconectado.")
-
-    return redirect('/login')
 
 # Inicia servidor
 if __name__ == "__main__":
